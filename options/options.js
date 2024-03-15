7c45023195694c48e51d331b54e5aab6920c30a7
Crowdbotics <engineering@crowdbotics.com>
2024-03-15 01:53:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "diy-cooking-1",
+  url: "https://diy-cooking-1.botics.co",
+  api: "https://diy-cooking-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
